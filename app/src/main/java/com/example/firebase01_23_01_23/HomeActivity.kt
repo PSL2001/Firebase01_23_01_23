ce0486c87b81de5071d6439a067c9d253d3a5bd0
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2023-01-23 14:34:37 +0100

    Creado menu para el home activity

--- app/src/main/java/com/example/firebase01_23_01_23/HomeActivity.kt
+++ app/src/main/java/com/example/firebase01_23_01_23/HomeActivity.kt
@@ -2,10 +2,42 @@ package com.example.firebase01_23_01_23
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.firebase01_23_01_23.databinding.ActivityHomeBinding
+import com.example.firebase01_23_01_23.prefs.Prefs
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+    private lateinit var prefs: Prefs
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        prefs = Prefs(this)
+        val email = prefs.leerEmail().toString()
+        binding.tvEmail.text = email
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_option, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_logout -> {
+                FirebaseAuth.getInstance().signOut()
+                prefs.borradoTotal()
+                finish()
+                true
+            }
+            R.id.item_exit -> {
+                finishAffinity()
+                true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
